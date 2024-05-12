a81f0d0514202eb657974ce6dfb746520ab519fd
Gabriel Díaz <gdiaz@qswarm.com>
2024-05-12 19:51:21 +0200

    fix how ints are converted to str in params

--- lib/param.ml
+++ lib/param.ml
@@ -5,6 +5,6 @@ let with_param k v params = (k, v) :: params
 let remove_param k params = List.filter (fun p -> not (String.equal k p)) params
 let bool_to_string b = if b then "true" else "fasle"
 let float_to_string f = Format.sprintf "%10f" f
-let int_to_string i = Format.sprintf "%10d" i
+let int_to_string i = Format.sprintf "%d" i
 
 exception ParamTypeNotSupported of string
